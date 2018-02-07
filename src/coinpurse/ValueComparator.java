bfe54b8f4096a6bbc7ce399a819b8671252b578b
Charin Tantrakul <charin.ta@ku.th>
2018-02-07 19:23:23 +0700

    Edit ValueComparator

--- src/coinpurse/ValueComparator.java
+++ src/coinpurse/ValueComparator.java
@@ -19,9 +19,11 @@ public class ValueComparator implements Comparator<Valuable> {
 		if (a.getCurrency().equalsIgnoreCase(b.getCurrency())) {
 			if (a.getValue() == b.getValue())
 				return 0;
-			if (b.getValue() - a.getValue() < 0)
+			if (b.getValue() > a.getValue() )
+				return -1;
+			if(b.getValue() < a.getValue() )
 				return 1;
 		}
-		return -1;
+		return a.getCurrency().compareTo(b.getCurrency());
 	}
 }
